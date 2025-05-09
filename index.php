e55a7aa61486996d9e666c5d4281bf07b01d4fdb
adam99610 <77136810+adam99610@users.noreply.github.com>
2025-05-09 16:57:24 +0100

    Added index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+include(__DIR__ . "../includes/config.php");
+include('includes/header.php');
+require_once(__DIR__ . "/includes/functions.php");
+
+?>
+<h1>Welcome to BookNest</h1>
+<p>Your gateway to new opportunities.</p>
+
+<?php include("includes/footer.php"); ?>
